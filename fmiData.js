3480e5008b3d10684ff1ca5be0fef53680f80389
mikkoro <mikko.ronkkomaki@solita.fi>
2015-02-14 13:39:31 +0200

    Building up forecast points from FMI data

--- fmiData.js
+++ fmiData.js
@@ -1,11 +1,20 @@
 var httpsync = require('httpsync');
+var dateUtils = require('date-utils');
 
 function get(){
-	var url = 'http://data.fmi.fi/fmi-apikey/55d44f5f-70e8-4d88-9d5e-ee083dcd0ca3/wfs?request=getFeature&storedquery_id=fmi::forecast::hirlam::surface::cities::timevaluepair&place=oulu&starttime=2015-02-07T00:00:00Z&endtime=2015-02-08T00:00:00Z&parameters=windspeedms,temperature';
+	var startTime = Date.today().toISOString().replace(".000", "");
+	var endTime = Date.today().add({days: 3}).toISOString().replace(".000", "");
+
+	console.log("Fetching FMI data");
+	console.log("Start time: " + startTime);
+	console.log("End time: " + endTime);
+
+	var parameters = 'windspeedms,winddirection,windGust,temperature';
+	var url = 'http://data.fmi.fi/fmi-apikey/55d44f5f-70e8-4d88-9d5e-ee083dcd0ca3/wfs?request=getFeature&storedquery_id=fmi::forecast::hirlam::surface::point::simple&place=oulu&starttime=' + startTime + '&endtime=' + endTime + '&parameters='+parameters;
+	console.log(url);
 
 	var req = httpsync.get({ url : url});
 	var res = req.end();
-	console.log(res)
 
 	return res.data;
 }
--- forecasts.js
+++ forecasts.js
@@ -1,7 +1,55 @@
 var fmiData = require("./fmiData");
+var parseString = require('xml2js').parseString;
+var _ = require('lodash');
 
 function get(){
-	return fmiData.get();
+	var xml = fmiData.get();
+	var data = parseAsObject(xml);
+	var forecasts = buildForecastPoints(data);
+	var json = JSON.stringify(forecasts);
+
+	//console.log(json);
+
+	return json;
 }
+
+function parseAsObject(xml){
+	var data;
+	parseString(xml, function (err, result) {
+	    var json = JSON.stringify(result);
+	    data = JSON.parse(json);
+	}); 
+	return data;
+}
+
+function buildForecastPoints(data){
+	var forecastPoints = {};
+
+	_.forEach(data["wfs:FeatureCollection"]["wfs:member"], function(forecast, index) { 
+		var timePoint = forecast["BsWfs:BsWfsElement"][0]['BsWfs:Time'];
+		
+		if(forecastPoints[timePoint] === undefined){
+			forecastPoints[timePoint] = {};
+		}
+
+		var attributeName = forecast["BsWfs:BsWfsElement"][0]["BsWfs:ParameterName"];
+		var attributeValue = forecast["BsWfs:BsWfsElement"][0]["BsWfs:ParameterValue"];
+
+		console.log(attributeName);
+
+		if (attributeName == "windspeedms") {   
+	        forecastPoints[timePoint].windspeed = attributeValue;
+	    } else if (attributeName == "winddirection"){
+	        forecastPoints[timePoint].winddirection = attributeValue;
+		} else if (attributeName == "windGust"){
+	        forecastPoints[timePoint].windgust = attributeValue;
+		} else if (attributeName == "temperature"){
+	        forecastPoints[timePoint].temperature = attributeValue;
+		}
+	});
+
+	return forecastPoints;
+}
+
 
 exports.get = get;
